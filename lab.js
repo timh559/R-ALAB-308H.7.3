1ba2d1aed6363ccad22cb8d657c275995fc6c1c4
Tim Hutchens <103814770+timh559@users.noreply.github.com>
2023-06-06 09:44:45 -0700

    created Person class

--- lab.js
+++ lab.js
@@ -15,3 +15,41 @@ class Hamster {
         return this.price;
     }
 }
+class Person {
+
+    constructor(name) {
+        this.name = name;
+        this.age = 0;
+        this.height = 0;
+        this.weight = 0;
+        this.mood = 0;
+        this.hamsters = [];
+        this.bankAccount = 0;
+    }
+    getName() {
+        return this.name;
+    }
+    getAge() {
+        return this.age;
+    }
+    getWeight() {
+        return this.weight;
+    }
+    greet() {
+        console.log(`Hello ${this.name}`);
+    }
+    eat() {
+        this.weight += 10;
+        this.mood += 5;
+    }
+    exercise() {
+        this.weight -= 5;
+    }
+    ageUp(num) {
+        this.age += (1 * num);
+        this.height += (3 * num);
+        this.weight += (2 * num);
+        this.mood -= (2 * num);
+        this.bankAccount += (10 * num);
+    }
+}
