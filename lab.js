ce8baf602d291003a5296bf7cefc9cd39c4a73e8
Tim Hutchens <103814770+timh559@users.noreply.github.com>
2023-06-06 09:30:52 -0700

    created Hamster class

---
+++ lab.js
@@ -0,0 +1,17 @@
+class Hamster {
+
+    constructor(name) {
+        this.name = name;
+        this.owner = "";
+        this.price = 15;
+    }
+    wheelRun() {
+        console.log("squeak squeak");
+    }
+    eatFood() {
+        console.log("nibble nibble");
+    }
+    getPrice() {
+        return this.price;
+    }
+}
